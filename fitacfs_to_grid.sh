88c44ef69c8f62eabc55e550eb9584bbd479ff49
ellioday <elliott@dayabode.plus.com>
2021-02-16 13:39:55 +0000

    added fitacfs_to_grid.sh

---
+++ fitacfs_to_grid.sh
@@ -0,0 +1,32 @@
+
+help(){
+
+	echo "This merges all .fitacf files for one date for a single radar and converts it into a .grd file"
+
+	echo
+	echo "inputs:"
+	echo "3-letter station code (AAA)"
+	echo "date (YYYY/MM/DD)"
+}
+
+#get inputs
+rad=$1
+date=$2
+year=${date:0:4}
+month=${date:5:2}
+day=${date:8:2}
+
+echo "${year} ${month} ${day}"
+
+#get path to .fitacf files
+luna_path=$(</home/elliott/Documents/python_analysis/luna_path.txt)
+fitacfs_path="${luna_path}users/daye1/Superdarn/Data/fitacf/$rad/$year/$month/"
+fitacf_name="${year}${month}${day}.*.${rad}.fitacf"
+echo "$fitacfs_path$fitacf_name"
+#get path to store .grd file
+grd_path="${luna_path}users/daye1/Superdarn/Data/grid/$rad/$year/$month/"
+grd_name="${year}${month}${day}.${rad}.grd"
+echo "$grd_path$grd_name"
+
+#merge and convert .fitacf files into .grd
+make_grid -vb -tl 60 -xtd -c "$fitacfs_path$fitacf_name" > "$grd_path$grd_name" 
--- get_all_fitacf.sh
+++ get_all_fitacf.sh
@@ -1,3 +1,15 @@
+help(){
+
+	echo "This scipt retrieves all fitacf data between two dates for all stations off of the luna drive (and un bz2 zips them)"
+
+	echo
+	echo "inputs:"
+	echo "start_date (YYYY/MM/DD_hh/mm/ss)"
+	echo "end_date (YYY/MM/DD_hh/mm/ss)"
+	echo "verbose level (0/1/2 default 0)"
+	echo "number of initial stations to skip retrieval (default 0)"
+}
+
 #array of all superdarn stations
 stations=("ade" "adw" "bks" "cly" "cve" "cvw" "fhe" "fhw" "gbr" "han" "hkw" "hok" "inv" "kap" "kod" "ksr" "lyr" "pgr" "pyk" "rkn" "sas" "sto" "wal")
 #get requested date (must be in format "YYYY/MM/DD hh:mm:ss")
